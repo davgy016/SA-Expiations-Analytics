103044fe40254d13646519fe959b0a727c274ec9
davgy016 <133870761+davgy016@users.noreply.github.com>
2024-10-23 19:10:47 +1030

    created suburb list for for dashboard

---
+++ Assig2/a2reactapp/src/components/SuburbList.js
@@ -0,0 +1,30 @@
+﻿import React, { useState, useEffect } from 'react';
+
+function SuburbList() {
+    const [suburbs, setSuburbs] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:5147/api/Get_ListCameraSuburbs")
+            .then(response => response.json())
+            .then(data => setSuburbs(data))
+            .catch(err => {
+                console.log(err);
+            });
+    }, [])
+
+    return (
+        <div className="dropdown">
+            <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                Select Suburb
+            </button>
+            <ul className="dropdown-menu overflow-auto" aria-labelledby="dropdownMenuButton" style={{maxHeight: '400px'} }>
+                {suburbs.map((suburb, index) =>(
+               
+                    <li key={index}><a className="dropdown-item" href="#"></a>{suburb}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default SuburbList;
